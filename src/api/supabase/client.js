2020def92e9226493f2f8f6f10343aa8eca434f3
CodedBeats <luca.haar@icloud.com>
2026-01-03 23:19:56 +1100

    fix: better handling for supabase client init func

--- src/api/supabase/client.js
+++ src/api/supabase/client.js
@@ -1,8 +1,13 @@
 import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
+let client
+
 export function createClient() {
-    return createSupabaseClient(
-        import.meta.env.VITE_SUPABASE_URL,
-        import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY
-    )
+    if (!client) {
+        client = createSupabaseClient(
+            import.meta.env.VITE_SUPABASE_URL,
+            import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY
+        )
+    }
+    return client
 }

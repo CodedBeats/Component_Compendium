144d655089a26020269925c7fd6a4394780f5d33
CodedBeats <luca.haar@icloud.com>
2026-01-07 00:26:15 +1100

    style: better website title

--- src/components/titles/WebsiteTitle.jsx
+++ src/components/titles/WebsiteTitle.jsx
@@ -6,7 +6,9 @@ import { RxComponent1 } from "react-icons/rx";
 export const WebsiteTitle = () => {
     return (
         <div className={styles.container}>
-            <RxComponent1 className={styles.websiteIcon} />
+            <div className={styles.iconContainer}>
+                <RxComponent1 className={styles.websiteIcon} />
+            </div>
             <h1 className={styles.websiteTitle}>Component Compendium</h1>
         </div>
     );

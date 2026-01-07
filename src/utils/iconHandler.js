7e74b6d8e7afa578824912df9ce195da4877d924
CodedBeats <luca.haar@icloud.com>
2026-01-07 17:04:57 +1100

    feat: icon handler for 1 import and export

---
+++ src/utils/iconHandler.js
@@ -0,0 +1,19 @@
+// import icons
+import { RiLockPasswordFill as PasswordLockIcon } from "react-icons/ri";
+import { MdMarkEmailRead as EmailTickIcon } from "react-icons/md";
+import { RxComponent1 as ComponentIcon } from "react-icons/rx";
+import { 
+    FaGithub as GithubIcon, 
+    FaEyeSlash as EeyeSlashIcon,
+    FaEye as EyeIcon
+} from "react-icons/fa";
+
+// export icons
+export {
+    PasswordLockIcon,
+    EmailTickIcon,
+    GithubIcon,
+    EeyeSlashIcon,
+    EyeIcon,
+    ComponentIcon
+}

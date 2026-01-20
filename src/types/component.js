5eb0e4c001f7f0663f84335e5eb5bf34c75e2239
CodedBeats <luca.haar@icloud.com>
2026-01-20 23:30:56 +1100

    feat: typedefs for component jsdoc

---
+++ src/types/component.js
@@ -0,0 +1,26 @@
+
+/**
+ * @typedef {Object} ComponentsByCategoryData
+ * @property {number} id
+ * @property {string} name
+ * @property {string} description
+ * @property {Date} created_at
+ */
+
+
+/**
+ * @typedef {Object} ComponentSimpleData
+ * @property {number} id
+ * @property {string} name
+ * @property {string} description
+ * @property {Date} created_at
+ */
+
+
+/**
+ * @typedef {Object} ComponentCodeVariantsData
+ * @property {number} id
+ * @property {string} name
+ * @property {string} description
+ * @property {Date} created_at
+ */

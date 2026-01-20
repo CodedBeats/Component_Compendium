4cd6dffb390282a7d82db412a86b09e4c5c35df9
CodedBeats <luca.haar@icloud.com>
2026-01-20 23:11:40 +1100

    feat: typedefs moved into their own folder

---
+++ src/types/user.js
@@ -0,0 +1,16 @@
+
+/**
+ * @typedef {Object} UserFabouriteCategoriesWithCountsData
+ * @property {number} user_id
+ * @property {number} category_id
+ * @property {string} category_name
+ * @property {number} component_count
+ */
+
+/**
+ * @typedef {Object} UserAllCategoriesWithCountsData
+ * @property {number} user_id
+ * @property {number} category_id
+ * @property {string} category_name
+ * @property {number} component_count
+ */

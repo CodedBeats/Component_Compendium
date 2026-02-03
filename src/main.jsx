a22101054e90a83e81cff79af06e47cf2bedefc6
CodedBeats <luca.haar@icloud.com>
2026-02-03 23:20:40 +1100

    docs: added tanstack dev tools to app

--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,7 @@ createRoot(document.getElementById("root")).render(
             <AuthProvider>
                 <AuthBoundry>
                     <App />
+                    <ReactQueryDevtools />
                 </AuthBoundry>
             </AuthProvider>
         </QueryClientProvider>
